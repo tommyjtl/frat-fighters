8f108b206c1fa954991826163857170b2b8ea0fd
GiovZa <zavalzagiovanni@gmail.com>
2025-05-21 16:59:29 -0500

    Cutscene revamped
    Plays each cutscene panel after 3 seconds or key press
    
    Refer to:
    https://docs.google.com/presentation/d/1ltOWJapx4ZslkYVa95itpHua7JUrdBEUDYeJpxcHBM8/edit?slide=id.g35a7c048945_0_29#slide=id.g35a7c048945_0_29

---
+++ Assets/Osarion/BeatEmUpTemplate2D/Scripts/Cutscene/CutsceneManager.cs
@@ -0,0 +1,132 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
+
+public class CutsceneManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class PanelGroup
+    {
+        public List<GameObject> panels;
+    }
+
+    [Header("Panel Groups")]
+    public List<PanelGroup> panelGroups = new List<PanelGroup>();
+
+    [Header("Scene to Load After Cutscene")]
+    public string nextSceneName;
+
+    [Header("Panel Timing")]
+    public float panelDisplayInterval = 3f; // seconds between panels
+
+    private int currentGroupIndex = 0;
+    private int panelIndexInGroup = 0;
+    private float panelTimer = 0f;
+
+    private InputAction anyKeyAction;
+
+    void Awake()
+    {
+        anyKeyAction = new InputAction(type: InputActionType.Button, binding: "<Keyboard>/anyKey");
+        anyKeyAction.performed += ctx => HandleInput();
+    }
+
+    void OnEnable()
+    {
+        anyKeyAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        anyKeyAction.Disable();
+    }
+
+    void Start()
+    {
+        DisableAllPanels();
+        ShowCurrentPanel();
+    }
+
+    void Update()
+    {
+        if (currentGroupIndex >= panelGroups.Count)
+            return;
+
+        var group = panelGroups[currentGroupIndex].panels;
+
+        // If not at last panel, auto-progress after delay
+        if (panelIndexInGroup < group.Count - 1)
+        {
+            panelTimer += Time.deltaTime;
+
+            if (panelTimer >= panelDisplayInterval)
+            {
+                panelIndexInGroup++;
+                ShowCurrentPanel();
+                panelTimer = 0f;
+            }
+        }
+    }
+
+    void HandleInput()
+    {
+        if (currentGroupIndex >= panelGroups.Count)
+            return;
+
+        var group = panelGroups[currentGroupIndex].panels;
+
+        // If not all panels shown, reveal next panel & reset timer
+        if (panelIndexInGroup < group.Count - 1)
+        {
+            panelIndexInGroup++;
+            ShowCurrentPanel();
+            panelTimer = 0f;
+        }
+        else
+        {
+            // All panels shown → go to next group or load scene
+            if (currentGroupIndex == panelGroups.Count - 1)
+            {
+                SceneManager.LoadScene(nextSceneName);
+            }
+            else
+            {
+                DisableAllInGroup(currentGroupIndex);
+                currentGroupIndex++;
+                panelIndexInGroup = 0;
+                panelTimer = 0f;
+                ShowCurrentPanel();
+            }
+        }
+    }
+
+    void ShowCurrentPanel()
+    {
+        var group = panelGroups[currentGroupIndex].panels;
+
+        for (int i = 0; i <= panelIndexInGroup && i < group.Count; i++)
+        {
+            group[i].SetActive(true);
+        }
+    }
+
+    void DisableAllPanels()
+    {
+        foreach (var group in panelGroups)
+        {
+            foreach (var panel in group.panels)
+            {
+                panel.SetActive(false);
+            }
+        }
+    }
+
+    void DisableAllInGroup(int groupIndex)
+    {
+        foreach (var panel in panelGroups[groupIndex].panels)
+        {
+            panel.SetActive(false);
+        }
+    }
+}
